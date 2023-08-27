61e7eed77390fa68ae000477c1a0ae6a33aa4b14
Suraj Bastola <55536711+bastolasuraj@users.noreply.github.com>
2023-08-27 13:36:07 -0400

    initial upload
    This is the first entry point

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <script src="script.js" defer></script>
+    <title>My Card</title>
+</head>
+<body>
+    <div class="card">
+        <div class="header">
+            <h1>suraj bastola</h1>
+        </div>
+        <div class="info">
+            <p><span>Email:</span> bastolasuraj@outlook.com</p>
+            <p><span>Phone:</span> 6479368128</p>
+            <p><span>LinkedIn:</span> <a href="https://linkedin.com/in/bastolasuraj" target="_blank">LinkedIn Profile</a></p>
+            <p><span>Website:</span> <a href="https://sbastola.com" target="_blank">sbastola.com</a></p>
+        </div>
+        <div class="actions">
+            <div class="work-experience">
+                <img src="work-icon.png" alt="Work Experience" class="icon">
+                <p>Work Experience</p>
+            </div>
+            <div class="education">
+                <img src="education-icon.png" alt="Education" class="icon">
+                <p>Education</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const workExperience = document.querySelector(".work-experience");
+    const education = document.querySelector(".education");
+    
+    workExperience.addEventListener("click", () => {
+        // Show lightbox pop effect for work experience
+        // You can implement this using CSS and JavaScript libraries like Lightbox or Modal.
+    });
+    
+    education.addEventListener("click", () => {
+        // Show lightbox pop effect for education
+    });
+});
